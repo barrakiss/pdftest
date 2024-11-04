38ceafb79cc9d5227e2bed21bbaaee3b81af4e40
czaszkobij <manowski.b@gmail.com>
2024-11-04 23:49:20 +0100

    html 2 pdf fix

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,46 +2,44 @@
   <div class="about">
     <h1>This is an about page</h1>
     <button @click="generateReport()">generate</button>
-    <!--    <vue3-html2pdf-->
-    <!--      :show-layout="false"-->
-    <!--      :float-layout="true"-->
-    <!--      :enable-download="true"-->
-    <!--      :preview-modal="true"-->
-    <!--      :paginate-elements-by-height="1400"-->
-    <!--      filename="hee hee"-->
-    <!--      :pdf-quality="2"-->
-    <!--      :manual-pagination="false"-->
-    <!--      pdf-format="a4"-->
-    <!--      pdf-orientation="landscape"-->
-    <!--      pdf-content-width="800px"-->
-    <!--      ref="html2Pdf"-->
-    <!--    >-->
-    <!--      &lt;!&ndash;      @progress="onProgress($event)"&ndash;&gt;-->
-    <!--      &lt;!&ndash;      @hasStartedGeneration="hasStartedGeneration()"&ndash;&gt;-->
-    <!--      &lt;!&ndash;      @hasGenerated="hasGenerated($event)"&ndash;&gt;-->
-    <!--      <template v-slot:pdf-content>-->
-    <!--        <section>-->
-    <!--          &lt;!&ndash; PDF Content Here &ndash;&gt;-->
-    <!--          <h1>dupa jasia</h1>-->
-    <!--        </section>-->
-    <!--      </template>-->
-    <!--    </vue3-html2pdf>-->
+    <vue3-html2pdf
+      :show-layout="false"
+      :float-layout="true"
+      :enable-download="true"
+      :preview-modal="true"
+      :paginate-elements-by-height="1400"
+      filename="hee hee"
+      :pdf-quality="2"
+      :manual-pagination="false"
+      pdf-format="a4"
+      pdf-orientation="landscape"
+      pdf-content-width="800px"
+      ref="html2Pdf"
+    >
+      <!--      @progress="onProgress($event)"-->
+      <!--      @hasStartedGeneration="hasStartedGeneration()"-->
+      <!--      @hasGenerated="hasGenerated($event)"-->
+      <template v-slot:pdf-content>
+        <section>
+          <!-- PDF Content Here -->
+          <h1>dupa jasia</h1>
+        </section>
+      </template>
+    </vue3-html2pdf>
   </div>
 </template>
 
 <script lang="ts">
-// import * as Vue3Html2pdf from 'vue3-html2pdf'
-// import Vue3Html2pdf from 'vue3-html2pdf'
-// import { Vue3Html2pdf } from 'vue3-html2pdf'
+import Vue3Html2pdf from "vue3-html2pdf";
 
 export default {
   components: {
-    // Vue3Html2pdf,
+    Vue3Html2pdf,
   },
   methods: {
     generateReport() {
       console.log("=>(AboutView.vue:36) generateReport");
-      // this.$refs.html2Pdf.generatePdf()
+      this.$refs.html2Pdf.generatePdf();
     },
   },
 };
